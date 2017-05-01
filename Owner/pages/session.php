f41b04076191c621a92d0e69401c33c5f99fb3aa
CruthOver <b3ng.b3ng16@gmail.com>
2017-05-02 02:51:32 +0700

    Revert "Membuat Database dan Menampilkan Data di Owner"
    This reverts commit 6aea676eec078e34b36d4367b283a1c9416d3afe.

--- Owner/pages/session.php
+++ Owner/pages/session.php
@@ -1,5 +1,7 @@
 <?php
 	include("../../Connections.php");
+	$user_check = $_SESSION['owner'];
+	$sql = mysqli_query($conn,"select username from owner where username = '$user_check' ");
 	if(!isset($_SESSION['owner'])){
 		header("location:login.php");
 	}
